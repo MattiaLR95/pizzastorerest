4b6bbf237ab0bf17ab28d7f52c4485ceb8a96f21
MattiaLR95 <laroccamattia@virgilio.it>
2023-01-08 12:51:20 +0100

    Implementato Cliente Repository

---
+++ src/main/java/it/prova/pizzastorerest/repository/cliente/ClienteRepository.java
@@ -0,0 +1,18 @@
+package it.prova.pizzastorerest.repository.cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.pizzastorerest.model.Cliente;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long>, CustomClienteRepository {
+
+	@Query("from Cliente c left join fetch c.ordini o where c.id = :id")
+	Optional<Cliente> findByIdEager(Long id);
+
+	@Query("from Cliente c where c.attivo = 1")
+	List<Cliente> listAll();
+}
