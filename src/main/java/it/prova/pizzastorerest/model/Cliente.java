a3ee7ec3b335deb7c56d69f11dbb82beeb725fca
MattiaLR95 <laroccamattia@virgilio.it>
2023-01-08 12:42:59 +0100

    Creazione model

---
+++ src/main/java/it/prova/pizzastorerest/model/Cliente.java
@@ -0,0 +1,45 @@
+package it.prova.pizzastorerest.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "indirizzo")
+	private String inidirizzo;
+	@Column(name = "attivo")
+	private boolean attivo;
+
+	@Builder.Default
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente")
+	private List<Ordine> ordini = new ArrayList<>();
+}
---
+++ src/main/java/it/prova/pizzastorerest/model/Ruolo.java
@@ -0,0 +1,39 @@
+package it.prova.pizzastorerest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ruolo")
+public class Ruolo {
+
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_PIZZAIOLO = "ROLE_PIZZAIOLO";
+	public static final String ROLE_PROPRIETARIO = "ROLE_PROPRIETARIO";
+	public static final String ROLE_FATTORINO = "ROLE_FATTORINO";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "codice")
+	private String codice;
+	@Column(name = "descrizione")
+	private String descrizione;
+
+}
